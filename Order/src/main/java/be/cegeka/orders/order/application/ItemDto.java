6eb77f801e8c1715be332b825238e6b1a7b9ff60
Vancampenhoudt Ralph <ralph.vancampenhoudt@cegeka.com>
2017-11-10 14:29:48 +0100

    fixed itemservicetest

--- Order/src/main/java/be/cegeka/orders/order/application/ItemDto.java
+++ Order/src/main/java/be/cegeka/orders/order/application/ItemDto.java
@@ -6,4 +6,13 @@ public class ItemDto {
     public String name;
     public String description;
     public BigDecimal sellingPrice;
+
+    public ItemDto() {
+    }
+
+    public ItemDto(String name, String description, BigDecimal sellingPrice) {
+        this.name = name;
+        this.description = description;
+        this.sellingPrice = sellingPrice;
+    }
 }
