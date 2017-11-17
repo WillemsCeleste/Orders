19f6a585633076558ad8eba3d261b011d9b98efa
Celeste Willems <willems.celeste@gmail.com>
2017-11-17 09:27:55 +0100

    something on ordertest

--- Order/src/test/java/be/cegeka/orders/order/domain/orders/OrderTest.java
+++ Order/src/test/java/be/cegeka/orders/order/domain/orders/OrderTest.java
@@ -43,7 +43,8 @@ public class OrderTest {
     }
 
     @Test
-    public void equals() throws Exception {
+    public void AnOrderShouldBeEqualToAnotherOrderWithSameId() throws Exception {
+
     }
 
 }
