5c82d7dcbfa516b608a83cf71ff09a6fd5aa46cb
Vancampenhoudt Ralph <ralph.vancampenhoudt@cegeka.com>
2017-11-17 14:32:03 +0100

    addad AttributeConverterTest

---
+++ Supplier/src/test/java/be/cegeka/orders/supplier/domain/LocalDateTimeAttributeConverterTest.java
@@ -0,0 +1,31 @@
+//package be.cegeka.orders.supplier.domain;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import java.sql.Timestamp;
+//import java.time.LocalDate;
+//import java.time.LocalDateTime;
+//
+//import static org.junit.Assert.*;
+//
+//public class LocalDateTimeAttributeConverterTest {
+//
+//    private LocalDateTime localDateTime;
+//    private Timestamp sqlTimestamp;
+//
+//    @Before
+//    public void setUp() throws Exception {
+//        localDateTime = LocalDateTime.
+//    }
+//
+//    @Test
+//    public void convertToDatabaseColumn_shouldConvertLocalDateToTimeStamp() throws Exception {
+//
+//    }
+//
+//    @Test
+//    public void convertToEntityAttribute() throws Exception {
+//    }
+//
+//}
