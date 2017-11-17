85ff9d1a29094ba97167f31b79261915ee8c2a76
Gerri Vanderhaegen <gerri.vanderhaegen@cegeka.com>
2017-11-17 09:27:08 +0100

    order equals completely tested

--- Order/src/test/java/be/cegeka/orders/order/domain/orders/OrderTest.java
+++ Order/src/test/java/be/cegeka/orders/order/domain/orders/OrderTest.java
@@ -54,5 +54,14 @@ public class OrderTest {
         Assertions.assertThat(order).isEqualTo(order);
     }
 
-    
+    @Test
+    public void anOrder_equalsNullObject_shouldBeFalse() throws Exception {
+        Assertions.assertThat(order).isNotEqualTo(null);
+    }
+
+    @Test
+    public void anOrderWithADifferentId_shouldNotBeEqual() throws Exception {
+        Order order2 = new Order(Date.valueOf(LocalDate.of(2017, 11, 11)),itemgroup);
+        Assertions.assertThat(order).isNotEqualTo(order2);
+    }
 }
